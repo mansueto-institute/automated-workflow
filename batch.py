537f4a4f1310ae298463f8d5f272041133ca61b3
Andrew Warfield <andrew.warfield@protonmail.com>
2022-06-22 13:23:47 -0500

    remove action form argparse

--- batch.py
+++ batch.py
@@ -9,11 +9,6 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    # options for control flow
-    parser.add_argument('action', choices=['upload','download', 'spot'],
-                        help='Upload/Download to an AWS bucket or \
-                        run compute.py in an AWS spot instance.')
-
     # always required to authenticate
     parser.add_argument('--access', dest = 'access', required=True,
                         help='The AWS users access key.')
