5224b9479da51a43a109e20f64710f5d133a6d52
Andrew Warfield <andrew.warfield@protonmail.com>
2022-11-08 08:29:49 -0600

    R env proof of concept

---
+++ rjob.R
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+
+
+library(tidyverse)
+library(aws.s3)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+
+Sys.setenv("AWS_ACCESS_KEY_ID" = args[1],
+           "AWS_SECRET_ACCESS_KEY" = args[2],
+           "AWS_DEFAULT_REGION" = "us-east-2")
+
+read <- read_csv('https://data.cityofchicago.org/api/views/bbt9-ihrm/rows.csv?accessType=DOWNLOAD')
+
+read['MODDED'] = read['WARD'] * read['DATES']
+
+s3write_using(read, FUN = write.csv,
+              bucket = args[3],
+              object = "RJOB.csv")
